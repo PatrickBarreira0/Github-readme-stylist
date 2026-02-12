122a8f2ea189463c972318487c08f8d8c5349215
PatrickBarreira0 <patrick.barreira@cittamobi.com.br>
2026-02-12 00:39:39 -0300

    test new git action5

--- packages/core/build.js
+++ packages/core/build.js
@@ -9,23 +9,5 @@ await esbuild.build({
   format: 'cjs',
   loader: {
     '.flf': 'text'
-  },
-  banner: {
-    js: `
-// Polyfills for bundled code
-if (typeof __dirname === 'undefined') {
-  global.__dirname = '.';
-}
-if (typeof __filename === 'undefined') {
-  global.__filename = 'index.js';
-}
-if (typeof import.meta === 'undefined') {
-  global.import = { meta: { url: 'file:///' + __dirname + '/' + __filename } };
-}
-    `.trim()
-  },
-  mainFields: ['module', 'main'],
-  define: {
-    'import.meta.url': '"file:///dist/index.js"'
   }
 });
